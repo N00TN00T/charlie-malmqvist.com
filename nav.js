6eb833e8710a5997cb1261ca6511ba6592eea39d
Charlie Malmqvist <charlie.challe@live.se>
2020-05-02 20:19:24 +0200

    a

--- nav.js
+++ nav.js
@@ -1,18 +1,22 @@
-var segments = window.location.pathname.split('/');
-var toDelete = [];
-for (var i = 0; i < segments.length; i++) {
-    if (segments[i].length < 1) {
-        toDelete.push(i);
-    }
-}
-for (var i = 0; i < toDelete.length; i++) {
-    segments.splice(i, 1);
-}
-var filename = segments[segments.length - 1];
-
-var filenameraw = filename.split('.')[0];
-
 $(function(){ $(".nav_top").load("nav.html"); });
 
-console.log(filenameraw);
-document.getElementById(filenameraw).classList.add("nav-current");
+window.onload = function()
+{
+    var segments = window.location.pathname.split('/');
+    var toDelete = [];
+    for (var i = 0; i < segments.length; i++) {
+        if (segments[i].length < 1) {
+            toDelete.push(i);
+        }
+    }
+    for (var i = 0; i < toDelete.length; i++) {
+        segments.splice(i, 1);
+    }
+    var filename = segments[segments.length - 1];
+
+    var filenameraw = filename.split('.')[0];
+
+
+    console.log(filenameraw);
+    document.getElementById(filenameraw).classList.add("nav-current");
+}
