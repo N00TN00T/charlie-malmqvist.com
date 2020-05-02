90cc4d85de17124d8cae5adb3548b571f76e9d7d
Charlie Malmqvist <charlie.challe@live.se>
2020-05-02 20:11:05 +0200

    a

--- nav.js
+++ nav.js
@@ -1,1 +1,15 @@
+var segments = window.location.pathname.split('/');
+var toDelete = [];
+for (var i = 0; i < segments.length; i++) {
+    if (segments[i].length < 1) {
+        toDelete.push(i);
+    }
+}
+for (var i = 0; i < toDelete.length; i++) {
+    segments.splice(i, 1);
+}
+var filename = segments[segments.length - 1];
+
 $(function(){ $(".nav_top").load("nav.html"); });
+
+document.getElementById(filename).classList.add("nav-current");
